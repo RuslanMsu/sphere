2ceca66f1dbbf789d1cdd36b077fc4e803ed708b
Ruslan Ostapets <ostapetc1@yandex.ru>
2021-03-09 02:14:40 +0300

    Init

---
+++ sphere/src/sphere.cpp
@@ -0,0 +1,27 @@
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n;
+	cin >> n;
+	int t;
+	int count = 0;
+	int count_min = 0;
+	for(int i = 0; i < n; i++){
+		cin >> t;
+		if(t==5){
+			count++;
+		}else{
+			if(t-5 <= count*5){
+				count-=(t-5)/5;
+			}else{
+				count_min += ((t-5) - count*5)/5;
+				count = 0;
+			}
+		}
+
+	}
+	cout << count_min;
+	return 0;
+}
